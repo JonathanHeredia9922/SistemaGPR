2202322640becbdb18347bf7600946b837a7c1da
JonathanHeredia9922 <159564142+JonathanHeredia9922@users.noreply.github.com>
2024-10-13 20:55:14 -0600

    MVP

---
+++ SistemaGPR/app/views.py
@@ -0,0 +1,28 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
+def registro(request):
+    
+    if request.method == 'GET':
+          return render(request, 'registro.html', {
+        'form': UserCreationForm 
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registro de usuarion
+            try:
+                user = User.objects.create_user(username=request.POST['username'], 
+                                     password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado correctamente')
+            except:
+                return HttpResponse('El ususario ya existe')
+        return HttpResponse('Las contraseñas no coinciden')
